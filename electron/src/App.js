77ba35abdb3773c0f333754bc788bb91b7757eee
JFriel <james.friel4@gmail.com>
2018-06-03 21:19:37 +0100

    can get messages, need an efficient way to scan all blocks

--- electron/src/App.js
+++ electron/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.web3 = null;
-    this.state = {'connected': false, latestBlock:{'hash': null}}
+    this.state = {'connected': false, latestBlock:{'hash': null}, messages:['t1','t2']}
   }
 
   connectToProvider() {
@@ -16,22 +16,40 @@ class App extends Component {
     try {
 	let web3 = new Web3(new Web3.providers.HttpProvider(provider));
         this.web3 = web3;  
-        web3.eth.net.isListening() ? this.setState({connected:true}) : this.setState({connected: false});
+        web3.net.listening ? this.setState({connected:true}) : this.setState({connected: false});
     } catch (err) {
         console.log(err);
 	this.setState({connected: false});
     }
   }
- 
+  
+
+
   getBlock(){
     // should return the latest block in the chain
-    const block = this.web3.eth.getBlock(this.web3.eth.blockNumber, true);
-    this.setState({'latestBlock': block});
-    console.log(block);
+    const block = this.web3.eth.getBlock('latest', true);
+    //this.setState({'latestBlock': block});
+    //console.log(block);
+    return block;
   }
-	
+
+  getMessages(){
+    if(this.web3 != null){
+    let block = this.getBlock()
+    if (block != this.state.latestBlock){
+      this.setState({'latestBlock': block});
+      console.log(block);
+      if (block.transactions.length !== 0) {
+        block.transactions.map(t => {
+	  console.log(t);
+	  console.log(this.web3.utils.hexToAscii(t.input));
+          //this.setState({'messages': this.state.messages.append(t.
+	});
+      }
+    }}
+  }
+
   render() {
-    console.log(config);
     return (
       <div className="App">
         <p>
@@ -39,9 +57,11 @@ class App extends Component {
 	    account: {config.accountID} <br/>
 	    connected: {this.state.connected.toString()}<br/>
 	    latestBlock: {this.state.latestBlock.hash}
+
+	    {this.state.messages.map(message => {return (<div>{message}</div>)})}
 	</p>
 	    <button onClick={() => {this.connectToProvider()}}>Connect</button>
-	    <button onClick={() => {this.getBlock()}}>GetBlock</button>
+	    <button onClick={() => {setInterval(()=>{this.getMessages()}, 3000);}}>get Messages</button>
       </div>
     );
   }
